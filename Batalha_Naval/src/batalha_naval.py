2c78887b6784a49d36d44b915f288150e35bed9b
Marcelo Bernardo <marcelobernardo84@gmail.com>
2015-10-14 16:39:49 -0300

    para nao perder

---
+++ Batalha_Naval/src/batalha_naval.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "marcelo"
+__date__ = "$14/10/2015 16:39:18$"
+
+if __name__ == "__main__":
+    print "Hello World"
