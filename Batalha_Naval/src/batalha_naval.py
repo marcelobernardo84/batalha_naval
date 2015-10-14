fd2920106255176ae209a053de6a3271beb594bc
Marcelo Bernardo <marcelobernardo84@gmail.com>
2015-10-14 16:42:43 -0300

    Batalha Naval Codeacademy

--- Batalha_Naval/src/batalha_naval.py
+++ Batalha_Naval/src/batalha_naval.py
@@ -2,8 +2,51 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-__author__ = "marcelo"
-__date__ = "$14/10/2015 16:39:18$"
+from random import randint
 
-if __name__ == "__main__":
-    print "Hello World"
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Vamos jogar Batalha Naval!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+# Tudo a partir daqui deve ir dentro do laco!
+# Tenha certeza de recuar quatro espacos!
+for turn in range(4):
+    guess_row = int(raw_input("Adivinhe a Linha:"))
+    guess_col = int(raw_input("Adivinhe a Coluna:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Parabens, voce afundou meu couracado!"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, isso nao e nem mesmo no oceano."            
+        elif(board[guess_row][guess_col] == "X"):
+            print "Joce ja tentou esse."
+        else:
+            print "Voce errou meu couracado!"
+            board[guess_row][guess_col] = "X"
+        if turn == 3:
+            print "Game Over"
+            board[ship_row][ship_col] = "T"
+            print_board(board)
+            break
+    # Exiba aqui (turn + 1)!
+    print "Turn: " + str(turn + 1)
+    print_board(board)
